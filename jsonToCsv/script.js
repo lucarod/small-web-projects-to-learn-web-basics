edc4fc92af14b381ee725bc0ad9e894c47fa9695
Luca Rodrigues <luca12rodrigues@gmail.com>
2021-05-18 15:47:17 -0300

    refactor : optimized code with javascript methods

--- jsonToCsv/script.js
+++ jsonToCsv/script.js
@@ -1,55 +1,43 @@
-const csvOutput = document.querySelector('#csvOutput');
+// Thanks, Marcos, for the help with this one!
+
+const csvOutput = document.querySelector("#csvOutput");
 
 function tryConvert(inputText) {
-    content = inputText.trim();
-    if(!checkJSON(content)) {
-        alert("Erro");
-    } else{
-        convertJSON(content);
-    }
+  const jsonContentString = inputText.trim();
+  if (!checkJSON(jsonContentString)) {
+    alert("Error");
+    return;
+  }
+  const convertedCsv = convertJsonToCsv(jsonContentString);
+  csvOutput.innerHTML = convertedCsv;
 }
 
-function checkJSON(content) {
-    try {
-        JSON.parse(content);
-        return true;
-    } catch (error) {
-        return false;
-    }
+function checkJSON(jsonContentString) {
+  try {
+    JSON.parse(jsonContentString);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
-function convertJSON(content) {
-    const parsedJSON = JSON.parse(content);
+function convertJsonToCsv(jsonContentString) {
+  const parsedJSON = JSON.parse(jsonContentString);
 
-    const jsonArray = Array.isArray(parsedJSON) ?  parsedJSON : [parsedJSON];
+  const jsonArray = Array.isArray(parsedJSON) ? parsedJSON : [parsedJSON];
 
-    const header = jsonArray[0];
-    
-    const keysArray = Object.keys(header);
+  const [firstObject] = jsonArray;
 
-    let csvValue = "";
+  const csvHeaders = Object.keys(firstObject);
 
-    keysArray.forEach((key) => {
-        if (keysArray.indexOf(key) === keysArray.length - 1) {
-            csvValue += `${key}`
-        } else csvValue += `${key}, `
-    });
+  const csvRows = jsonArray.map((object) =>
+    Object.entries(object)
+      .filter(([key, value]) => csvHeaders.includes(key))
+      .map(([key, value]) => value)
+      .join(", ")
+  );
 
-    csvValue += "\n"
+  const csvContent = [csvHeaders.join(", "), ...csvRows].join("\n");
 
-    jsonArray.forEach((object) => {
-        const valuesArray = Object.values(object);
-
-        valuesArray.forEach((value) => {
-            if(valuesArray.indexOf(value) <= keysArray.length - 1){
-                if (valuesArray.indexOf(value) === keysArray.length - 1) {
-                    csvValue += `${value}`
-                } else csvValue += `${value}, `
-            }
-        })
-
-        csvValue += "\n"
-    })
-
-    csvOutput.innerHTML = csvValue;
+  return csvContent;
 }
