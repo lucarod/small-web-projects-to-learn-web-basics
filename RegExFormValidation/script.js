0122a0b1dcac31ea542f82a207e958fd29ee2323
Luca Rodrigues <luca12rodrigues@gmail.com>
2021-05-22 17:51:36 -0300

    feat and refactor! (RegExFormValidator): Added modal success screen and changed html password requirements text

--- RegExFormValidation/script.js
+++ RegExFormValidation/script.js
@@ -4,38 +4,52 @@ const password = document.getElementById('password');
 
 const invalidFieldMessage = document.querySelectorAll('.check')
 
-function checkValidation(event) {
-    const patterns = {
-        emailPattern: /^(\S+)@((?:(?:(?!-)[a-zA-Z0-9-]{1,62}[a-zA-Z0-9])\.)+[a-zA-Z0-9]{2,12})$/,
-        usernamePattern: /^[a-zA-Z]+$/,
-        passwordPattern: /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*()+\-.,;?\^.,;?><:{}\[\]])[\w!@#$%&*()+\-.,;?\^.,;?><:{}\[\]]{6,12}$/
+const Modal = {
+    toggle() {
+        document.querySelector(".modal-overlay").classList.toggle("active")
     }
-
-    if(patterns.emailPattern.test(email.value)) {
-        invalidFieldMessage[0].classList.remove('error-message')
-    } else {
-        event.preventDefault();
-        invalidFieldMessage[0].classList.add('error-message');
-    }
-
-    if(patterns.usernamePattern.test(username.value)) {
-        invalidFieldMessage[1].classList.remove('error-message')
-    } else {
-        event.preventDefault();
-        invalidFieldMessage[1].classList.add('error-message')
-    }
-
-    if(patterns.passwordPattern.test(password.value)) {
-        invalidFieldMessage[2].classList.remove('error-message')
-    } else {
-        event.preventDefault();
-        invalidFieldMessage[2].classList.add('error-message')
-    }
-
-    // if (patterns.emailPattern.test(email.value) && patterns.usernamePattern.test(username.value) && patterns.passwordPattern.test(password.value)) return
-
-    // event.preventDefault();
-    // if(!patterns.emailPattern.test(email.value)) invalidFieldMessage[0].classList.add('error-message')
-    // if(!patterns.usernamePattern.test(username.value)) invalidFieldMessage[1].classList.add('error-message')
-    // if(!patterns.passwordPattern.test(password.value)) invalidFieldMessage[2].classList.add('error-message')
 }
+
+const Form = {
+    checkValidation(event) {
+        event.preventDefault();
+    
+        const patterns = {
+            emailPattern: /^(\S+)@((?:(?:(?!-)[a-zA-Z0-9-]{1,62}[a-zA-Z0-9])\.)+[a-zA-Z0-9]{2,12})$/,
+            usernamePattern: /^[a-zA-Z]+$/,
+            passwordPattern: /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*()+\-.,;?\^.,;?><:{}\[\]])[\w!@#$%&*()+\-.,;?\^.,;?><:{}\[\]]{6,12}$/
+        }
+    
+        if(patterns.emailPattern.test(email.value)) {
+            invalidFieldMessage[0].classList.remove('error-message')
+        } else {
+            invalidFieldMessage[0].classList.add('error-message');
+        }
+    
+        if(patterns.usernamePattern.test(username.value)) {
+            invalidFieldMessage[1].classList.remove('error-message')
+        } else {
+            invalidFieldMessage[1].classList.add('error-message')
+        }
+    
+        if(patterns.passwordPattern.test(password.value)) {
+            invalidFieldMessage[2].classList.remove('error-message')
+        } else {
+            invalidFieldMessage[2].classList.add('error-message')
+        }
+    
+        if (patterns.emailPattern.test(email.value) && patterns.usernamePattern.test(username.value) && patterns.passwordPattern.test(password.value)) Modal.toggle();
+    
+        // event.preventDefault();
+        // if(!patterns.emailPattern.test(email.value)) invalidFieldMessage[0].classList.add('error-message')
+        // if(!patterns.usernamePattern.test(username.value)) invalidFieldMessage[1].classList.add('error-message')
+        // if(!patterns.passwordPattern.test(password.value)) invalidFieldMessage[2].classList.add('error-message')
+    },
+
+    clearFields() {
+        email.value = "";
+        username.value = "";
+        password.value = "";
+    }
+}
+
