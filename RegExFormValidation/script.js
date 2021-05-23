e5e128a54e7f6c15f6e58e23dc68fabcd9bdfb9a
Luca Rodrigues <luca12rodrigues@gmail.com>
2021-05-23 11:47:26 -0300

    refactor!(RegExFormValidator): Optimize javascript code

--- RegExFormValidation/script.js
+++ RegExFormValidation/script.js
@@ -1,8 +1,8 @@
-const email = document.getElementById('email');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-
-const invalidFieldMessage = document.querySelectorAll('.check')
+const formFields = {
+    email: document.getElementById('email'),
+    username: document.getElementById('username'),
+    password: document.getElementById('password'),
+}
 
 const Modal = {
     toggle() {
@@ -10,40 +10,98 @@ const Modal = {
     }
 }
 
+function showMessageError(inputEl) {
+    const messageErrorEl = inputEl.parentElement.querySelector('.check')
+    messageErrorEl.classList.add('error-message')
+}
+
+function clearMessageError(inputEl) {
+    inputEl
+        .parentElement
+        .querySelector('.check')
+        .classList
+        .remove('error-message')
+}
+
+function clearAllErrors() {
+    clearMessageError(email)
+    clearMessageError(username)
+    clearMessageError(password)
+}
+
 const Form = {
     checkValidation(event) {
         event.preventDefault();
-    
-        const patterns = {
-            emailPattern: /^(\S+)@((?:(?:(?!-)[a-zA-Z0-9-]{1,62}[a-zA-Z0-9])\.)+[a-zA-Z0-9]{2,12})$/,
-            usernamePattern: /^[a-zA-Z]+$/,
-            passwordPattern: /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*()+\-.,;?\^.,;?><:{}\[\]])[\w!@#$%&*()+\-.,;?\^.,;?><:{}\[\]]{6,12}$/
+        clearAllErrors();
+
+        /**
+         * Ao menos um caractere maiusculo
+         * Ao menos um símbolo
+         * Pelo menos 6 dígitos e no máximo 12 digitos
+         * Ao menos um número
+         * 
+         * schema : shape
+         * 
+         * schema validation libs: Yup; Joi 
+         */
+
+        const validate = {
+            email: () => {
+                const emailPattern = /^(\S+)@((?:(?:(?!-)[a-zA-Z0-9-]{1,62}[a-zA-Z0-9])\.)+[a-zA-Z0-9]{2,12})$/
+
+                const isValid = emailPattern.test(formFields.email.value)
+
+                if (!isValid) {
+                    showMessageError(email)
+                }
+
+                return isValid;
+            },
+            username: () => {
+               const  usernamePattern = /^[a-zA-Z]+$/
+
+               const isValid = usernamePattern.test(formFields.username.value)
+               
+               if (!isValid) {
+                    showMessageError(username)    
+               }
+
+               return isValid;
+            },
+            password: () => {
+                const checkCapitalize = /[A-Z]/g;
+                const checkSymbol = /[!@#$%&*()+\-.,;?\^.,;?><:{}\[\]]/g;
+                const checkNumber = /[0-9]/;
+
+                const isValid = checkCapitalize.test(formFields.password.value)
+                    && checkSymbol.test(formFields.password.value)
+                    && checkNumber.test(formFields.password.value)
+                    && (password.value.length >= 6 || password.value.length <= 12)
+
+                if (!isValid) {
+                    showMessageError(password)
+                }
+
+                return isValid;
+            },
         }
-    
-        if(patterns.emailPattern.test(email.value)) {
-            invalidFieldMessage[0].classList.remove('error-message')
-        } else {
-            invalidFieldMessage[0].classList.add('error-message');
+
+        const isFormValid = Object.keys(formFields).reduce((isValid,field) => { 
+            const result = validate[field]()
+
+            if (!isValid) {
+                return false;
+            }
+
+            return result;
+        }, true)
+
+        if (isFormValid){
+            // chamar uma API para enviar os dados do formulário
+            // featch api
+            // axios
+            Modal.toggle();
         }
-    
-        if(patterns.usernamePattern.test(username.value)) {
-            invalidFieldMessage[1].classList.remove('error-message')
-        } else {
-            invalidFieldMessage[1].classList.add('error-message')
-        }
-    
-        if(patterns.passwordPattern.test(password.value)) {
-            invalidFieldMessage[2].classList.remove('error-message')
-        } else {
-            invalidFieldMessage[2].classList.add('error-message')
-        }
-    
-        if (patterns.emailPattern.test(email.value) && patterns.usernamePattern.test(username.value) && patterns.passwordPattern.test(password.value)) Modal.toggle();
-    
-        // event.preventDefault();
-        // if(!patterns.emailPattern.test(email.value)) invalidFieldMessage[0].classList.add('error-message')
-        // if(!patterns.usernamePattern.test(username.value)) invalidFieldMessage[1].classList.add('error-message')
-        // if(!patterns.passwordPattern.test(password.value)) invalidFieldMessage[2].classList.add('error-message')
     },
 
     clearFields() {
@@ -52,4 +110,3 @@ const Form = {
         password.value = "";
     }
 }
-
